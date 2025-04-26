ba6a764f7f3d275380f1b1c6041fe26db450f5ea
jwc920 <jwc920github@gmail.com>
2025-04-26 10:06:12 -0700

    update keymap

--- keyboards/bastardkb/scylla/keymaps/jason-scylla/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/jason-scylla/keymap.c
@@ -20,34 +20,43 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_4x6_5(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_0,    KC_9,    KC_8,    KC_7,    KC_6,    KC_CAPS,
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_P,    KC_O,    KC_I,    KC_U,    KC_Y,    OSM(MOD_LSFT),
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_SCLN, KC_L,    KC_K,    KC_J,    KC_H,    KC_ENT,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_SLSH, KC_DOT,  KC_COMM, KC_M,    KC_N,    KC_NO,
-                             OSL(3), KC_LGUI, OSM(MOD_LSFT), KC_TRNS, KC_LALT, TG(1), KC_ENT, KC_ESC, KC_BSPC, KC_SPC
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TG(1),
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_Y,    KC_B,    KC_CAPS,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_ENT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N, KC_M,  KC_COMM, KC_DOT,    KC_SLSH,    KC_NO,
+                             OSM(MOD_LSFT), KC_LGUI, OSL(3), KC_ENT, KC_SPC, KC_BSPC, KC_LALT, KC_LGUI, KC_ESC, KC_LGUI
     ),
 
     [1] = LAYOUT_split_4x6_5(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_F,    KC_P,    KC_D,    KC_L,    KC_X,             KC_SCLN, KC_B,    KC_Y,    KC_O,    KC_U,    KC_Z,
-        KC_TRNS, KC_S,    KC_N,    KC_T,    KC_H,    KC_K,             KC_QUOT, KC_C,    KC_I,    KC_E,    KC_A,    KC_Q,
-        KC_TRNS, KC_V,    KC_W,    KC_G,    KC_M,    KC_J,             KC_SLSH, KC_DOT,  KC_COMM, KC_MINS, KC_EQL,  KC_NO,
-                             KC_TRNS, KC_TRNS, KC_TRNS, LT(2, KC_R), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_F,    KC_P,    KC_D,    KC_L,    KC_X,             KC_SCLN, KC_U,    KC_O,    KC_Y,    KC_B,    KC_Z,
+        KC_TRNS, LCTL_T(KC_S),    LALT(KC_N),    LGUI_T(KC_T),    LSFT(KC_H),    KC_K,             KC_QUOT, LSFT_T(KC_A),    LGUI_T(KC_E),    LALT_T(KC_I),    LCTL_T(KC_C),    KC_Q,
+        KC_TRNS, KC_V,    KC_W,    KC_G,    KC_M,    KC_J,             KC_EQL, KC_MINS,  KC_COMM, KC_DOT, KC_SLSH,  KC_TRNS,
+                             KC_TRNS, LT(2, KC_R), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [2] = LAYOUT_split_4x6_5(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_END,  KC_UP,   KC_HOME, KC_TRNS,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS, KC_TRNS,          KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS,          KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KC_TRNS, KC_TRNS,
         KC_TRNS, G(KC_Z), G(KC_X), G(KC_C), G(KC_V), KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [3] = LAYOUT_split_4x6_5(
         KC_TRNS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, S(KC_7), S(KC_LBRC), S(KC_MINS), S(KC_RBRC), KC_GRV, KC_BSLS, S(KC_BSLS), S(KC_SCLN), S(KC_8), S(KC_7), KC_TRNS,
-        KC_TRNS, S(KC_6), S(KC_9), KC_EQL, S(KC_0), S(KC_SLSH),        KC_SCLN, S(KC_QUOT), KC_QUOT, KC_MINS, S(KC_GRV), KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_LBRC, S(KC_EQL), KC_RBRC, KC_TRNS,       KC_SLSH, S(KC_DOT), KC_MINS, S(KC_COMM), KC_TRNS, KC_TRNS,
+        KC_TRNS, S(KC_7), S(KC_LBRC), S(KC_MINS), S(KC_RBRC), KC_GRV, S(KC_7), S(8), S(KC_SCLN), S(KC_BSLS), KC_BSLS, KC_TRNS,
+        KC_TRNS, S(KC_6), S(KC_9), KC_EQL, S(KC_0), S(KC_SLSH),        S(KC_GRV), KC_MINS, KC_QUOT, S(KC_QUOT), KC_SCLN, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_LBRC, S(KC_EQL), KC_RBRC, KC_TRNS,       KC_TRNS, S(KC_COMM), KC_MINS, S(KC_DOT), KC_SLSH, KC_TRNS,
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
+
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT(
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+             '*', '*', '*', '*','*','*','*','*', '*', '*'
+    );
